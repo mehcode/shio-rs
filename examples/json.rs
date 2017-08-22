ad4eaada4107f6f496f722a906d670a4afa3a97c
Ryan Leckey <leckey.ryan@gmail.com>
2017-08-21 17:23:27 -0700

    Use 'serde_json::from_async_reader' in json example

--- examples/json.rs
+++ examples/json.rs
@@ -2,38 +2,16 @@
 
 extern crate salt;
 extern crate futures;
-extern crate tokio_io;
 extern crate serde;
 
 #[macro_use]
 extern crate serde_json;
 
 #[macro_use]
-extern crate error_chain;
-
-#[macro_use]
 extern crate serde_derive;
 
-mod errors {
-    error_chain! {
-        foreign_links {
-            Io(::std::io::Error);
-            Json(::serde_json::Error);
-        }
-    }
-}
-
-use tokio_io::io;
 use futures::future;
 use salt::prelude::*;
-use errors::Error;
-
-// TODO: Contribute this as serde_json::from_async_reader
-// fn json_from_async_reader<'a, T: serde::de::Deserialize<'a>, R: io::AsyncRead>(reader: R) -> T {
-//     io::read_to_end(reader, Vec::new()).from_err::<Error>().and_then(|(_, buffer)| {
-//         future::done(serde_json::from_slice(&buffer)).from_err()
-//     })
-// }
 
 #[derive(Debug, Deserialize)]
 struct RequestBody {
@@ -41,22 +19,14 @@ struct RequestBody {
     name: String,
 }
 
-fn index(ctx: Context) -> impl Future<Item = Response, Error = Error> {
-    // TODO: Use serde_json::from_async_reader(..) when available
-    // serde_json::from_async_reader(ctx).map(|(_, body: RequestBody)| {
-
-    io::read_to_end(ctx, Vec::new()).from_err().and_then(|(_, buffer)| {
-        future::done(serde_json::from_slice(&buffer)).from_err()
-    }).map(|body: RequestBody| {
-        // TODO: Add a Responder so we can do:
-        //       Decide if we should have `Json` in a `salt_contrib` or not
-        //   Response::with(Json(json!( ... )))
-        //   Response::with(Json( ... ))
-
-        Response::with(serde_json::to_string(&json!({
+fn index<'a>(ctx: Context) -> impl Future<Item = Response> + 'a {
+    serde_json::from_async_reader(ctx).and_then(|(_, body): (_, RequestBody)| {
+        future::done(serde_json::to_string(&json!({
             "id": 20u8,
             "name": body.name,
-        }))).with_header(header::ContentType::json())
+        })))
+    }).map(|s| {
+        Response::with(s).with_header(header::ContentType::json())
     })
 }
 
