342172fc1cbda593124437d43579108707e40be2
Ryan Leckey <leckey.ryan@gmail.com>
2017-08-21 17:25:55 -0700

    Make json example compile

--- examples/json.rs
+++ examples/json.rs
@@ -28,13 +28,6 @@ use futures::future;
 use salt::prelude::*;
 use errors::Error;
 
-// TODO: Contribute this as serde_json::from_async_reader
-// fn json_from_async_reader<'a, T: serde::de::Deserialize<'a>, R: io::AsyncRead>(reader: R) -> T {
-//     io::read_to_end(reader, Vec::new()).from_err::<Error>().and_then(|(_, buffer)| {
-//         future::done(serde_json::from_slice(&buffer)).from_err()
-//     })
-// }
-
 #[derive(Debug, Deserialize)]
 struct RequestBody {
     visible: bool,
@@ -42,21 +35,15 @@ struct RequestBody {
 }
 
 fn index(ctx: Context) -> impl Future<Item = Response, Error = Error> {
-    // TODO: Use serde_json::from_async_reader(..) when available
-    // serde_json::from_async_reader(ctx).map(|(_, body: RequestBody)| {
-
     io::read_to_end(ctx, Vec::new()).from_err().and_then(|(_, buffer)| {
         future::done(serde_json::from_slice(&buffer)).from_err()
-    }).map(|body: RequestBody| {
-        // TODO: Add a Responder so we can do:
-        //       Decide if we should have `Json` in a `salt_contrib` or not
-        //   Response::with(Json(json!( ... )))
-        //   Response::with(Json( ... ))
-
-        Response::with(serde_json::to_string(&json!({
+    }).and_then(|body: RequestBody| {
+        future::done(serde_json::to_string(&json!({
             "id": 20u8,
             "name": body.name,
-        }))).with_header(header::ContentType::json())
+        }))).from_err()
+    }).map(|s| {
+        Response::with(s).with_header(header::ContentType::json())
     })
 }
 
