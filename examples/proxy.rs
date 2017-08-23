76ce11265fd8af01dd8b0aa1131850919e87dd28
Ryan Leckey <leckey.ryan@gmail.com>
2017-08-23 16:19:55 -0700

    Clean up proxy example, remove impl Trait usage

--- examples/proxy.rs
+++ examples/proxy.rs
@@ -1,4 +1,11 @@
-#![feature(conservative_impl_trait)]
+//! Proxy google and stream the response back to the client.
+//! This could be expanded into a simple http-proxy.
+
+// TODO: Expand this a bit.
+//  - Add some command line args: `proxy -p 7878 http://www.google.com`
+//  - Add log and log when a request is proxied
+//  - Use `Shio::new( .. )` as the router is just getting in the way here
+//  - Proxy full request path
 
 extern crate shio;
 extern crate hyper;
@@ -6,16 +13,13 @@ extern crate hyper;
 use shio::prelude::*;
 use hyper::Client;
 
-// Handlers can return a `shio::Response` or an `impl Future<Item = shio::Response>` (
-// which `impl FutureResponse` is an alias for).
-fn proxy_google(
-    ctx: Context,
-) -> impl Future<Item = Response, Error = impl ::std::fmt::Debug + Send> {
-    // Proxy google and stream the response back to the client
-    // This could easily be expanded into a simple http-proxy
+// Handlers return a `shio::Response` or a `shio::BoxFutureResponse`. The latter defaults the
+// error type to `Box<::std::error::Error>`.
+fn proxy_google(ctx: Context) -> BoxFutureResponse<hyper::Error> {
     Client::new(&ctx)
         .get("http://www.google.com".parse().unwrap())
         .map(|res| Response::build().body(res.body()))
+        .into_box()
 }
 
 fn main() {
