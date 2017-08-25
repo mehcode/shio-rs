679bc378040cd2c6327e635648793f297b8b7a1b
Ryan Leckey <leckey.ryan@gmail.com>
2017-08-25 00:02:32 -0700

    Add some more documentation to the proxy example

--- examples/proxy/src/main.rs
+++ examples/proxy/src/main.rs
@@ -13,16 +13,42 @@ extern crate shio;
 use shio::prelude::*;
 use hyper::Client;
 
-fn proxy_google(ctx: Context) -> BoxFuture<Response, hyper::Error> {
+fn proxy(ctx: Context) -> BoxFuture<Response, hyper::Error> {
+    // Additional work can be scheduled on the thread-local event loop,
+    // as each handler receives a reference to it
     Client::new(&ctx)
         .get("http://www.google.com".parse().unwrap())
+        // Map the _streaming_ response from google into a _streaming_
+        // response from us
         .map(|res| Response::build().body(res.body()))
+        // Use `.into_box` to turn this future stream into a `BoxFuture`
+        // that can be easily returned on stable Rust.
+        //
+        // When `impl Trait` becomes available on stable Rust, this
+        // necessity will go away
         .into_box()
 }
 
 fn main() {
-    Shio::default()
-        .route((Method::Get, "/", proxy_google))
-        .run(":7878")
-        .unwrap();
+    // Our simple HTTP proxy doesn't need a Router and Shio doesn't force
+    // a router on you.
+
+    // `Shio::new` expects a single root handler. By default this
+    // is a (wrapped) Router. As we don't need a router, we are using
+    // `Shio::new` to specify our own root handler.
+
+    Shio::new(proxy).run(":7878").unwrap();
+
+    // Here is an example of what `Shio::default` is equivalent to:
+    /*
+
+    Shio::new(
+      // Stack is a middleware container that executes each middleware
+      // around _its_ root handler
+      shio::Stack::new(shio::router::Router::new())
+        // By default, the Recover middleware is included in your stack
+        .with(shio::middleware::Recover)
+    )
+
+    */
 }
